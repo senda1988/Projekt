f2a5dbc403aa9b7cf4189e098ff2a91ad79dd071
Zsuzsa Farkas <zsuzsanna.farkas@tn.techstarter.de>
2024-12-20 10:53:37 +0100

    commit

--- Readme.md
+++ Readme.md
@@ -1,1 +1,5 @@
-Aufgabe
+# Projekt
+
+Aufgabe 1 - Weisong
+Aufgabe 2 - Zsuzsa
+Aufgabe 4 - Senda
---
+++ aufgabe2_zahl_erraten.py
@@ -0,0 +1,3 @@
+# Entwickelt ein Spiel, bei dem der Benutzer eine Zahl zwischen 1 und 10 erraten muss. Das
+# Programm sollte die richtige Zahl zufällig auswählen und den Benutzer darüber informieren,
+# ob er richtig geraten hat.
