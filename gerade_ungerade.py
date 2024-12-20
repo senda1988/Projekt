6610fd2ec527dc0504e292bf5467379f3b0f3ebd
senda1988 <zidi.senda@gmail.com>
2024-12-20 11:20:16 +0100

    Update gerade_ungerade.py

--- gerade_ungerade.py
+++ gerade_ungerade.py
@@ -1,6 +1,6 @@
 def gerade_ungerade():
     number = int(input("Schreiben Sie eine Zahl: "))
-    rest = number / 2
+    rest = number % 2
     if rest == 0:
         return print(f"Die Zahl {number} ist gerade")
     else:
